b43f9c813880774f2a2c8f696e6e14ae5631e401
Akodiat <joakimjo@swipnet.se>
2012-09-19 16:45:52 +0200

    Broadcasting ip-adress

--- Spang-PC_C-sharp/Spang-PC_C-sharp/Program.cs
+++ Spang-PC_C-sharp/Spang-PC_C-sharp/Program.cs
@@ -17,27 +17,17 @@ namespace Spang_PC_C_sharp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
-          //  Application.EnableVisualStyles();
-          //  Application.SetCompatibleTextRenderingDefault(false);
-          //  Application.Run(new Form1());
-
             Server server = new Server();
-            IConnection connection = server.ReciveConnection(1337);
-
-            // connection.connect(ADDRESS, PORT);
-
-            var messageHandler = MessageHandlerBuilder.Build();
-
             new Thread(() =>
             {
-                while (true)
-                {
-                    //connection.sendTCP(Encoding.UTF8.GetBytes(Console.ReadLine()));
-                }
+                Brodcast();
             }).Start();
+            
+            IConnection connection = server.ReciveConnection(1337);
+
+            var messageHandler = MessageHandlerBuilder.Build();
             
             while(true) 
             {
@@ -46,6 +36,23 @@ namespace Spang_PC_C_sharp
                 byte[] data = connection.reciveUDP();
                 messageHandler.DecodeMessage(data);   
 		    }
+        }
+        /// <summary>
+        /// http://www.java2s.com/Tutorial/CSharp/0580__Network/BroadcastSocketandbroadcastIPaddress.htm Assumed public licence
+        /// </summary>
+        private static void Brodcast()
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, 9999);
+
+            string hostname = Dns.GetHostName();
+            byte[] data = Encoding.ASCII.GetBytes(hostname);
+            while (true)
+            {
+                sock.SendTo(data, iep);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
