6261a6c385f769285de14847c9536e11c0719025
Gustav Alm Rosenblad <agustav@student.chalmers.se>
2012-09-19 10:19:40 +0200

    The mouseactivity now reacts to different touchevents (prints in logcat).

--- Spang-mobile/src/spang/mobile/MouseView.java
+++ Spang-mobile/src/spang/mobile/MouseView.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class MouseView extends View{
-	private Paint paint = new Paint();
-	private Path path = new Path();
+	private final Paint paint = new Paint();
+
+	private float previousTouchX;
+	private float previousTouchY;
+	private float dX;
+	private float dY;
+
+	private GestureDetector gestureDetector;
 
 	public MouseView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -21,28 +28,39 @@ public class MouseView extends View{
 		paint.setColor(Color.WHITE);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeJoin(Paint.Join.ROUND);
+		
+		 this.gestureDetector = new GestureDetector(context, simpleOnGestureListener);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		canvas.drawPath(path, paint);
+		canvas.drawLine(previousTouchX, previousTouchY, previousTouchX + dX, previousTouchY + dY, paint);
 	}
-
+	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		gestureDetector.onTouchEvent(event);
+		
 		float eventX = event.getX();
 		float eventY = event.getY();
 
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			path.moveTo(eventX, eventY);
+			previousTouchX = eventX;
+			previousTouchY = eventY;
 			return true;
 		case MotionEvent.ACTION_MOVE:
-			path.lineTo(eventX, eventY);
+			dX = previousTouchX + eventX;
+			dY = previousTouchY - eventY;
+			previousTouchX = eventX;
+			previousTouchY = eventY;
+
+			this.sendMovementData();
 			break;
 		case MotionEvent.ACTION_UP:
-			// nothing to do
-				break;
+			this.dX = 0;
+			this.dY = 0;
+			break;
 		default:
 			return false;
 		}
@@ -51,4 +69,41 @@ public class MouseView extends View{
 		invalidate();
 		return true;
 	}
+
+	private void sendMovementData() {
+		Log.d("MOTIONEVENT:", "dX = " + dX + "   dY = " + dY);
+	}
+
+	GestureDetector.SimpleOnGestureListener simpleOnGestureListener = new GestureDetector.SimpleOnGestureListener(){
+
+		public boolean onDown(MotionEvent e) {
+			Log.d("MOTIONEVENT:", "onDown");
+			return true;
+		}
+
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			Log.d("MOTIONEVENT:", "onFling");
+			return true;
+		}
+
+		public void onLongPress(MotionEvent e) {
+			Log.d("MOTIONEVENT:", "onLongPress");
+		}
+
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+				float distanceY) {
+			Log.d("MOTIONEVENT:", "onScroll");
+			return true;
+		}
+
+		public void onShowPress(MotionEvent e) {
+			Log.d("MOTIONEVENT:", "onShowPress");
+		}
+
+		public boolean onSingleTapUp(MotionEvent e) {
+			Log.d("MOTIONEVENT:", "onSingleTapUp");
+			return true;
+		}
+	};
 } 
