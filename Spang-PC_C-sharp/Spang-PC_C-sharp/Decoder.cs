75eec593afbd252460dac78b47e36af27dd9087f
Akodiat <joakimjo@swipnet.se>
2012-09-19 13:41:48 +0200

    Now you can controll the computer mouse and click (left)
    Also started decoding work

---
+++ Spang-PC_C-sharp/Spang-PC_C-sharp/Decoder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spang_PC_C_sharp
+{
+    class Decoder
+    {
+
+        public Accelerometer decodeAccelerometer(byte[] data)
+        {
+            float[] floatData = new float[3];
+            Buffer.BlockCopy(data, 1, floatData, 0, 12);
+            
+            return new Accelerometer
+            {
+                X = floatData[0],
+                Y = floatData[1],
+                Z = floatData[2]
+            };
+	    }
+    }
+}
--- Spang-PC_C-sharp/Spang-PC_C-sharp/Program.cs
+++ Spang-PC_C-sharp/Spang-PC_C-sharp/Program.cs
@@ -39,14 +39,41 @@ namespace Spang_PC_C_sharp
             
             while(true) 
             {
-                byte[] data = connection.reciveTCP();	
-			    string message = Encoding.UTF8.GetString(data, 0, data.Length);
+                byte[] data = connection.reciveUDP();	
+                string message = Encoding.UTF8.GetString(data, 0, data.Length);
                 Console.WriteLine(message);
-                if (message == "move")
+                string[] split = message.Split(';');
+
+                if(message.Contains(";"))
                 {
-                    Cursor.Position = new System.Drawing.Point(Cursor.Position.X + 10, Cursor.Position.Y - 10);
+                    int dx = int.Parse(split[0]);
+                    int dy = int.Parse(split[1]);
+                    Cursor.Position = new System.Drawing.Point(Cursor.Position.X - dx, Cursor.Position.Y - dy);
                 }
+                else if(message == "click")
+                {
+                    Console.WriteLine("click");
+                    MouseClick();
+                } 
 		    }
+        }
+
+        //Taken from http://social.msdn.microsoft.com/forums/en-US/winforms/thread/86dcf918-0e48-40c2-88ae-0a09797db1ab/ assumed public licence
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public static void MouseClick()
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
         }
     }
 }
