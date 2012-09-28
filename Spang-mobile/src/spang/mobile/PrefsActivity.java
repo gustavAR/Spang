ccd9fe058c454060bf86f229c50f5cacbc9dd25e
Akodiat <joakimjo@swipnet.se>
2012-09-28 15:27:14 +0200

    Dynamically adds settings for available sensors

--- Spang-mobile/src/spang/mobile/PrefsActivity.java
+++ Spang-mobile/src/spang/mobile/PrefsActivity.java
@@ -1,14 +1,49 @@
 package spang.mobile;
 
+import java.util.List;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 
 
 public class PrefsActivity extends PreferenceActivity{
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-   super.onCreate(savedInstanceState);
-   addPreferencesFromResource(R.xml.prefs);
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setPreferenceScreen(getPref());
+	}
+
+	private PreferenceScreen getPref() {
+		PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(this);
+
+		SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+
+
+		for (Sensor sensor : sensors) {
+			if(sensor != null){
+				String name = sensor.getName();
+				PreferenceCategory sensorCategory = new PreferenceCategory(this);
+				sensorCategory.setSummary(name);
+				sensorCategory.setTitle(name);
+				
+				screen.addPreference(sensorCategory);
+				
+				CheckBoxPreference checkbox = new CheckBoxPreference(this);
+				checkbox.setKey("checkBox"+name);
+				checkbox.setTitle("Activated");
+				checkbox.setSummary("Power usage: "+sensor.getPower()+"mA");
+
+				sensorCategory.addPreference(checkbox);
+				//screen.addPreference(checkbox);
+			}
+		}
+		return screen;
+	}
 }
