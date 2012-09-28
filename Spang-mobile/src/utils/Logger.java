eda875d4813489913ece332dcae92732167fe531
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-09-28 02:31:15 +0200

    Added a logger class and moved Packer to utils

---
+++ Spang-mobile/src/utils/Logger.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Simple logger class. That can be used to log exceptions and dubug information.
+ * @author Lukas
+ *
+ */
+public class Logger {
+		
+	
+	public static void LogException(Throwable throwable) {
+		//Tmp.
+		throwable.printStackTrace();
+	}
+}
