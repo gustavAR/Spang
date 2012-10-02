455c74a38f7a3bdd8dc0c6b98743498562c46c79
0st3n <pontus.pall@gmail.com>
2012-10-02 20:16:16 +0200

    TouchView now packs and sends its input

--- Spang-mobile/src/spang/mobile/TouchView.java
+++ Spang-mobile/src/spang/mobile/TouchView.java
@@ -20,17 +20,17 @@ public class TouchView extends View {
 	public boolean onTouchEvent(MotionEvent event) {
 		int pointers = event.getPointerCount();
 		
+		packer.pack((byte)100); //Temporary ID for touch packets
+		packer.pack((byte)pointers);
 		for(int i = 0; i < pointers; i++) {
-			float x = event.getX(i);
-			float y = event.getY(i);
-			float pressure = event.getPressure(i);	
-			int eventID = event.getActionMasked();
-			
-			//PACKA!
+			packer.pack(event.getX(i));
+			packer.pack(event.getY(i));
+			packer.pack(event.getPressure(i));	
+			packer.pack((event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> 
+			MotionEvent.ACTION_POINTER_INDEX_SHIFT);
 		}
-		
-		//SKICKA!
-		
+
+		service.sendUDP(packer.getPackedData());
 		return true;
 	}
 }
