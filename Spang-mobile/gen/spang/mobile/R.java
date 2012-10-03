d77791e193ad9160654faecf0a2c2772acb776a2
Akodiat <joakimjo@swipnet.se>
2012-10-03 13:34:28 +0200

    Added PrefsActivity to android manifest

--- Spang-mobile/gen/spang/mobile/R.java
+++ Spang-mobile/gen/spang/mobile/R.java
@@ -85,16 +85,17 @@ containing a value of this type.
     }
     public static final class string {
         public static final int app_name=0x7f060000;
-        public static final int button_send=0x7f060004;
-        public static final int default_ip=0x7f060006;
-        public static final int default_port=0x7f060007;
+        public static final int button_send=0x7f060005;
+        public static final int default_ip=0x7f060007;
+        public static final int default_port=0x7f060008;
         public static final int hello_world=0x7f060001;
         public static final int menu_settings=0x7f060002;
-        public static final int network_service_started=0x7f060009;
+        public static final int network_service_started=0x7f06000a;
         public static final int title_activity_main=0x7f060003;
-        public static final int title_activity_mouse=0x7f060005;
-        public static final int title_activity_networked=0x7f06000a;
-        public static final int title_activity_text_sender=0x7f060008;
+        public static final int title_activity_mouse=0x7f060006;
+        public static final int title_activity_networked=0x7f06000b;
+        public static final int title_activity_prefs=0x7f060004;
+        public static final int title_activity_text_sender=0x7f060009;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
