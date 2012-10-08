52dab698fcf58f9cceadce93ca13b4d87fdef8ae
Lukas Kurtyan <kurtyan@student.chalmers.se>
2012-10-08 23:04:33 +0200

    First untested implementation of the way handshake done.

--- Spang-PC_C-sharp/Spang-PC_C-sharp/ConnectionListener.cs
+++ Spang-PC_C-sharp/Spang-PC_C-sharp/ConnectionListener.cs
@@ -11,16 +11,60 @@ namespace Spang_PC_C_sharp
     {
         public IConnection ReciveConnection(int port)
         {
+
+            Connection connection;
+            while (true)
+            {
+                IPEndPoint endpoint = ReciveConnectionRequest(port);
+                UdpClient client = SendConnectionAck(endpoint);
+
+                if (RecivedConnectionAck(endpoint, client))
+                {
+                    connection = new Connection(client, endpoint);
+                    break;
+                }
+                else
+                {
+                    client.Close();
+                }
+            }
+
+            return connection;
+        }
+
+        private static bool RecivedConnectionAck(IPEndPoint endpoint, UdpClient client)
+        {
+            try
+            {
+                //Recive any message and we know that we have connected.
+                client.Receive(ref endpoint);
+                return true; 
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static UdpClient SendConnectionAck(IPEndPoint endpoint)
+        {
+            UdpClient client = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+            client.Connect(endpoint);
+
+            Packer packer = new Packer(4);
+            packer.Pack(((IPEndPoint)client.Client.LocalEndPoint).Port);      
+            client.Send(packer.GetPackedData(), 4);
+            return client;
+        }
+
+        private static IPEndPoint ReciveConnectionRequest(int port)
+        {
+
             UdpClient listener = new UdpClient(new IPEndPoint(IPAddress.Any, 1337));
             IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
             listener.Receive(ref endpoint);
             listener.Close();
-
-            UdpClient client = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
-            client.Connect(endpoint);
-            client.Send(BitConverter.GetBytes(((IPEndPoint)client.Client.LocalEndPoint).Port), 4);
-
-            return new Connection(client, endpoint);
+            return endpoint;
         }
     }
 }
--- Spang-mobile/src/network/Connector.java
+++ Spang-mobile/src/network/Connector.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import network.exceptions.NetworkException;
+import utils.UnPacker;
 
 public class Connector implements IConnector {
 
@@ -17,32 +19,49 @@ public class Connector implements IConnector {
 	public IConnection connect(InetSocketAddress address, int timeout) {
 		try {
 			DatagramSocket socket = new DatagramSocket();
-			//Sends a empty message signaling that we want to connect.
-			DatagramPacket dpacket = new DatagramPacket(new byte[0], 0, address);
-			socket.send(dpacket);
-							
-			//Receives a callback message giving endpoint specific information.
-			int remotePort = this.readOutgoingUdpPort(socket);
-			//Uses the received information to connect.
-			socket.connect(new InetSocketAddress(address.getAddress(), remotePort));		
+			// It should take no longer then 500 miliseconds to connect if it does we try again.
+			//socket.setSoTimeout(20000);
+			
+			sendConnectionRequest(address, socket);				
+			reciveConnectionInformation(address, socket);		
+			
+			sendConnectionAcknowledgement(socket);		
 		
+			
 			return new Connection(socket);
 		} catch (IOException e) {
 			throw new NetworkException("Could not connect");
 		}
 	}
 
+	private void reciveConnectionInformation(InetSocketAddress address,
+			DatagramSocket socket) throws IOException, SocketException {
+		//Receives a callback message giving endpoint specific information.
+		int remotePort = this.readOutgoingUdpPort(socket);
+		//Uses the received information to connect.
+		socket.connect(new InetSocketAddress(address.getAddress(), remotePort));
+	}
+
+	private void sendConnectionRequest(InetSocketAddress address,
+			DatagramSocket socket) throws SocketException, IOException {
+		
+		//Sends a empty message signaling that we want to connect.
+		DatagramPacket dpacket = new DatagramPacket(new byte[0], 0, address);
+		socket.send(dpacket);
+	}
+
 	private int readOutgoingUdpPort(DatagramSocket socket) throws IOException {
 		//Recives connection information package.
 		DatagramPacket dpacket = new DatagramPacket(new byte[4], 4);
 		socket.receive(dpacket);
-		
-		//Parses the package using LittleEndian byte order.
-		ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN);
-		buffer.put(dpacket.getData());
-		buffer.rewind();
-		
-		
-		return buffer.getInt();
+		//Unpack the contents of the package.
+		UnPacker unPacker = new UnPacker(dpacket.getData());	
+		return unPacker.unpackInt();
+	}
+
+	private void sendConnectionAcknowledgement(DatagramSocket socket) throws IOException {
+		//Send an empty message to let the endpoint know that we are ready to send and recive data.
+		DatagramPacket dpacket = new DatagramPacket(new byte[0], 0);
+		socket.send(dpacket);
 	}
 }
