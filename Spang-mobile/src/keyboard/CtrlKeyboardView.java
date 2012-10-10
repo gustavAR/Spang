1294a10058cc77b2289b23f712f85ff1d8bbe5cd
Akodiat <joakimjo@swipnet.se>
2012-10-10 13:54:35 +0200

    Fixed merge errors after merging with the master bransh

--- Spang-mobile/src/keyboard/CtrlKeyboardView.java
+++ Spang-mobile/src/keyboard/CtrlKeyboardView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package keyboard;
 
 import spang.mobile.NetworkService;
@@ -11,337 +10,164 @@ import android.util.AttributeSet;
 import android.util.Log;
 
 /**
- * This is the view which contains the keyboard.
- * The keyboard will send events to this view,
- * which sends them to the packer which will
- * send them over the network.
- * 
- * ATTENTION: setPacker(Packer packer) MUST be
- * 			  called with a valid packer before
- * 			  any keypresses are made.
- * @author Gustav Alm Rosenblad
- *
- */
+* This is the view which contains the keyboard.
+* The keyboard will send events to this view,
+* which sends them to the packer which will
+* send them over the network.
+*
+* ATTENTION: setPacker(Packer packer) MUST be
+* called with a valid packer before
+* any keypresses are made.
+* @author Gustav Alm Rosenblad
+*
+*/
 public class CtrlKeyboardView extends KeyboardView implements KeyboardView.OnKeyboardActionListener {
-
-	/**
-	 * This is the currently active keyboard,
-	 * the one we show.
-	 */
-	ControlKeyboard ctrlKeyboard;
-	
-	NetworkService network;
-	Packer packer;
-
-	private boolean ctrlActive; //Is ctrl currently pressed?
-	private boolean altgrActive;//Is altgr currently pressed?
-	private boolean shiftActive;//Is shift currently pressed?
-
-	/**
-	 * This constructor is mostly used by the android api.
-	 * @param context
-	 * @param attrs
-	 */
-	public CtrlKeyboardView(Context context, AttributeSet attrs, NetworkService networkService ) {
-		super(context, attrs);
-		this.ctrlKeyboard = new ControlKeyboard(context, R.xml.unicodeqwerty);
-		this.setKeyboard(ctrlKeyboard);
-		this.setOnKeyboardActionListener(this);
-		this.packer = new Packer();
-		this.network = networkService;
-	}
-
-	/**
-	 * This updates the appearance of the keyboard
-	 */
-	public void updateKeyboardState(){
-		if (ctrlActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
-		} else if (altgrActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodealtgr);
-		}
-		else if (this.shiftActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeshifted);
-		}
-		else {
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
-		}
-		this.setKeyboard(ctrlKeyboard);
-		ctrlKeyboard.ctrlKey.on = this.ctrlActive;
-		ctrlKeyboard.altgrKey.on = this.altgrActive;
-		this.setShifted(this.shiftActive);
-	}
-
-	public void swipeLeft() {}
-	public void swipeRight() {}
-
-	/**
-	 * Called when the user swipes up.
-	 * We will hide the keyboard when this happens.
-	 */
-	@Override
-	public void swipeUp() {
-		goBackToMain();
-	}
-
-	/**
-	 * Called when the user swipes down.
-	 * We will hide the keyboard when this happens.
-	 */
-	@Override
-	public void swipeDown() {
-		goBackToMain();
-	}
-
-	/**
-	 * Called when a key is pressed.
-	 */
-	public void onKey(int primaryCode, int[] keyCodes) {
-		/*this.client.sendTCP(
-				ByteBuffer.allocate(5).put((byte)15).putInt(primaryCode).array());*/
-		char character = (char)primaryCode;
-		switch (primaryCode){
-		case ControlKeyboard.SHIFT_KEYCODE:
-			this.shiftActive = !this.shiftActive;
-			break;
-		case ControlKeyboard.CTRL_KEYCODE:
-			this.ctrlActive = !this.ctrlActive;
-			break;
-		case ControlKeyboard.HIDE_KEYBOARD_KEYCODE:
-			goBackToMain();
-			break;
-		case ControlKeyboard.ALTGR_KEYCODE:
-			this.altgrActive = !this.altgrActive;
-			break;
-		default:
-			this.packer.packByte((byte)this.getContext().getResources().getInteger(R.integer.Text));
-			this.packer.packString(addModifierIDs(character));
-			this.network.send(this.packer.getPackedData());
-			this.packer.clear();
-			resetModifiers();
-			Log.i("CHAR", "" + (char)primaryCode);
-			}
-		this.updateKeyboardState();
-	}
-
-	private void resetModifiers() {
-		this.shiftActive = false;
-		this.altgrActive = false;
-		this.ctrlActive = false;
-	}
-
-	private String addModifierIDs(char character) {
-		String toSend = "";
-		if(this.shiftActive)//These values will probably not even be used in the final implementation
-			toSend += "\\shift";//TODO: Replace these ugly hardcoded values
-		if(this.ctrlActive)
-			toSend += "\\ctrl";//TODO: Replace these ugly hardcoded values
-		if(this.altgrActive)
-			toSend += "\\altgr";//TODO: Replace these ugly hardcoded values
-		toSend += character;
-		return toSend;
-	}
-
-	private void goBackToMain() {
-		((Activity)this.getContext()).finish();
-	}
-
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onPress(int primaryCode) {
-		
-	}
-	
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onRelease(int primaryCode) {
-		
-	}
-	
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onText(CharSequence text) {
-		
-	}
-}
-=======
-package keyboard;
-
-import spang.mobile.NetworkService;
-import spang.mobile.R;
-import utils.Packer;
-import android.app.Activity;
-import android.content.Context;
-import android.inputmethodservice.KeyboardView;
-import android.util.AttributeSet;
-import android.util.Log;
 
 /**
- * This is the view which contains the keyboard.
- * The keyboard will send events to this view,
- * which sends them to the packer which will
- * send them over the network.
- * 
- * ATTENTION: setPacker(Packer packer) MUST be
- * 			  called with a valid packer before
- * 			  any keypresses are made.
- * @author Gustav Alm Rosenblad
- *
- */
-public class CtrlKeyboardView extends KeyboardView implements KeyboardView.OnKeyboardActionListener {
+* This is the currently active keyboard,
+* the one we show.
+*/
+private ControlKeyboard ctrlKeyboard;
 
-	/**
-	 * This is the currently active keyboard,
-	 * the one we show.
-	 */
-	private ControlKeyboard ctrlKeyboard;
-	
-	private NetworkService network;
-	private Packer packer;
+private NetworkService network;
+private Packer packer;
 
-	private boolean ctrlActive; //Is ctrl currently pressed?
-	private boolean altgrActive;//Is altgr currently pressed?
-	private boolean shiftActive;//Is shift currently pressed?
+private boolean ctrlActive; //Is ctrl currently pressed?
+private boolean altgrActive;//Is altgr currently pressed?
+private boolean shiftActive;//Is shift currently pressed?
 
-	/**
-	 * This constructor is mostly used by the android api.
-	 * @param context
-	 * @param attrs
-	 */
-	public CtrlKeyboardView(Context context, AttributeSet attrs, NetworkService networkService ) {
-		super(context, attrs);
-		this.ctrlKeyboard = new ControlKeyboard(context, R.xml.unicodeqwerty);
-		this.setKeyboard(ctrlKeyboard);
-		this.setOnKeyboardActionListener(this);
-		this.packer = new Packer();
-		this.network = networkService;
-	}
-
-	/**
-	 * This updates the appearance of the keyboard
-	 */
-	public void updateKeyboardState(){
-		if (ctrlActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
-		} else if (altgrActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodealtgr);
-		}
-		else if (this.shiftActive){
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeshifted);
-		}
-		else {
-			ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
-		}
-		this.setKeyboard(ctrlKeyboard);
-		ctrlKeyboard.ctrlKey.on = this.ctrlActive;
-		ctrlKeyboard.altgrKey.on = this.altgrActive;
-		this.setShifted(this.shiftActive);
-	}
-
-	public void swipeLeft() {}
-	public void swipeRight() {}
-
-	/**
-	 * Called when the user swipes up.
-	 * We will hide the keyboard when this happens.
-	 */
-	@Override
-	public void swipeUp() {
-		goBackToMain();
-	}
-
-	/**
-	 * Called when the user swipes down.
-	 * We will hide the keyboard when this happens.
-	 */
-	@Override
-	public void swipeDown() {
-		goBackToMain();
-	}
-
-	/**
-	 * Called when a key is pressed.
-	 */
-	public void onKey(int primaryCode, int[] keyCodes) {
-		switch (primaryCode){
-		case ControlKeyboard.SHIFT_KEYCODE:
-			this.shiftActive = !this.shiftActive;
-			break;
-		case ControlKeyboard.CTRL_KEYCODE:
-			this.ctrlActive = !this.ctrlActive;
-			break;
-		case ControlKeyboard.HIDE_KEYBOARD_KEYCODE:
-			goBackToMain();
-			break;
-		case ControlKeyboard.ALTGR_KEYCODE:
-			this.altgrActive = !this.altgrActive;
-			break;
-		default:
-			char character = (char)primaryCode;
-			sendChar(character);
-			resetModifiers();
-			Log.i("CHAR", "" + (char)primaryCode);
-			}
-		this.updateKeyboardState();
-	}
-
-	private void sendChar(char character) {
-		this.packer.packByte((byte)this.getContext().getResources().getInteger(R.integer.Text));
-		this.packer.packString(addModifierIDs(character));
-		this.network.send(this.packer.getPackedData());
-		this.packer.clear();
-	}
-
-	private void resetModifiers() {
-		this.shiftActive = false;
-		this.altgrActive = false;
-		this.ctrlActive = false;
-	}
-
-	private String addModifierIDs(char character) {
-		String toSend = "";
-		if(this.shiftActive)//These values will probably not even be used in the final implementation
-			toSend += "\\shift";//TODO: Replace these ugly hardcoded values
-		if(this.ctrlActive)
-			toSend += "\\ctrl";//TODO: Replace these ugly hardcoded values
-		if(this.altgrActive)
-			toSend += "\\altgr";//TODO: Replace these ugly hardcoded values
-		toSend += character;
-		return toSend;
-	}
-
-	private void goBackToMain() {
-		((Activity)this.getContext()).finish();
-	}
-
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onPress(int primaryCode) {
-		
-	}
-	
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onRelease(int primaryCode) {
-		
-	}
-	
-	/**
-	 * We don't need to do anything here,
-	 * since everything is handled in onKey
-	 */
-	public void onText(CharSequence text) {
-		
-	}
+/**
+* This constructor is mostly used by the android api.
+* @param context
+* @param attrs
+*/
+public CtrlKeyboardView(Context context, AttributeSet attrs, NetworkService networkService ) {
+super(context, attrs);
+this.ctrlKeyboard = new ControlKeyboard(context, R.xml.unicodeqwerty);
+this.setKeyboard(ctrlKeyboard);
+this.setOnKeyboardActionListener(this);
+this.packer = new Packer();
+this.network = networkService;
 }
->>>>>>> origin/master
+
+/**
+* This updates the appearance of the keyboard
+*/
+public void updateKeyboardState(){
+if (ctrlActive){
+ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
+} else if (altgrActive){
+ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodealtgr);
+}
+else if (this.shiftActive){
+ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeshifted);
+}
+else {
+ctrlKeyboard = new ControlKeyboard(this.getContext(), R.xml.unicodeqwerty);
+}
+this.setKeyboard(ctrlKeyboard);
+ctrlKeyboard.ctrlKey.on = this.ctrlActive;
+ctrlKeyboard.altgrKey.on = this.altgrActive;
+this.setShifted(this.shiftActive);
+}
+
+public void swipeLeft() {}
+public void swipeRight() {}
+
+/**
+* Called when the user swipes up.
+* We will hide the keyboard when this happens.
+*/
+@Override
+public void swipeUp() {
+goBackToMain();
+}
+
+/**
+* Called when the user swipes down.
+* We will hide the keyboard when this happens.
+*/
+@Override
+public void swipeDown() {
+goBackToMain();
+}
+
+/**
+* Called when a key is pressed.
+*/
+public void onKey(int primaryCode, int[] keyCodes) {
+switch (primaryCode){
+case ControlKeyboard.SHIFT_KEYCODE:
+this.shiftActive = !this.shiftActive;
+break;
+case ControlKeyboard.CTRL_KEYCODE:
+this.ctrlActive = !this.ctrlActive;
+break;
+case ControlKeyboard.HIDE_KEYBOARD_KEYCODE:
+goBackToMain();
+break;
+case ControlKeyboard.ALTGR_KEYCODE:
+this.altgrActive = !this.altgrActive;
+break;
+default:
+char character = (char)primaryCode;
+sendChar(character);
+resetModifiers();
+Log.i("CHAR", "" + (char)primaryCode);
+}
+this.updateKeyboardState();
+}
+
+private void sendChar(char character) {
+this.packer.packByte((byte)this.getContext().getResources().getInteger(R.integer.Text));
+this.packer.packString(addModifierIDs(character));
+this.network.send(this.packer.getPackedData());
+this.packer.clear();
+}
+
+private void resetModifiers() {
+this.shiftActive = false;
+this.altgrActive = false;
+this.ctrlActive = false;
+}
+
+private String addModifierIDs(char character) {
+String toSend = "";
+if(this.shiftActive)//These values will probably not even be used in the final implementation
+toSend += "\\shift";//TODO: Replace these ugly hardcoded values
+if(this.ctrlActive)
+toSend += "\\ctrl";//TODO: Replace these ugly hardcoded values
+if(this.altgrActive)
+toSend += "\\altgr";//TODO: Replace these ugly hardcoded values
+toSend += character;
+return toSend;
+}
+
+private void goBackToMain() {
+((Activity)this.getContext()).finish();
+}
+
+/**
+* We don't need to do anything here,
+* since everything is handled in onKey
+*/
+public void onPress(int primaryCode) {
+
+}
+
+/**
+* We don't need to do anything here,
+* since everything is handled in onKey
+*/
+public void onRelease(int primaryCode) {
+
+}
+
+/**
+* We don't need to do anything here,
+* since everything is handled in onKey
+*/
+public void onText(CharSequence text) {
+
+}
+}
--- Spang-mobile/src/spang/mobile/NetworkedActivity.java
+++ Spang-mobile/src/spang/mobile/NetworkedActivity.java
@@ -15,96 +15,12 @@ import events.Action;
 import events.Action1;
 
 /**
-* Class that interfaces against the Network Service.
-* @author Lukas Kuryan.
-*
-*/
+ * Class that interfaces against the Network Service.
+ * @author Lukas Kuryan.
+ *
+ */
 public abstract class NetworkedActivity extends Activity {
 
-<<<<<<< HEAD
-private NetworkService network;	
-
-/**
-* Gets the active NetworkService.
-* NOTE: This will return null before onNetworkServiceConnected is called!
-* @return a NetworkService.
-*/
-public NetworkService getNetworkService() {	
-return this.network;
-}
-
-    @Override
-    protected void onStart() {
-     //Bind the service so we can use it.
-     Intent intent = new Intent(this, NetworkService.class);
-     this.bindService(intent, connection, Context.BIND_WAIVE_PRIORITY);	
-     super.onStart();
-    }
-    
-    @Override
-    protected void onStop() {
-     //Unbind the service so it can be closed later.
-     this.unbindService(connection);
-     super.onStop();
-    }
-    
-    //Connection used to bind the network service.
-    private ServiceConnection connection = new ServiceConnection() {
-
-public void onServiceDisconnected(ComponentName name) {
-network = null;
-NetworkedActivity.this.onNetworkServiceConnected();
-}
-
-public void onServiceConnected(ComponentName name, IBinder service) {
-network = ((NetworkService.NetworkBinder)service).getService();
-NetworkedActivity.this.onNetworkServiceConnected();
-
-//TODO implement multithreading problems. That is multithreading with the UI thread doing UI stuff.
-/*network.addConnectedListener(new EventHandler<IClient, Boolean>() {
-public void onAction(IClient sender, Boolean eventArgs) {
-NetworkedActivity.this.onConnected();
-}
-});
-network.addDisconnectedListener(new EventHandler<IClient, DCCause>() {
-public void onAction(IClient sender, DCCause eventArgs) {
-NetworkedActivity.this.onDisconnected(eventArgs);
-}
-});
-network.addRevicedListener(new EventHandler<IClient, byte[]>() {
-public void onAction(IClient sender, byte[] eventArgs) {
-NetworkedActivity.this.onMessageRecived(eventArgs);
-}
-});*/
-
-
-}
-};
-
-protected abstract void onNetworkServiceConnected();
-protected abstract void onNetworkSerivceDissconnected();
-protected abstract void onMessageRecived(byte[] message);
-
-
-protected void onConnected() {
-//Dunno what to do here atm :O
-}
-
-protected void onDisconnected(DCCause cause) {	
-AlertDialog.Builder builder = new AlertDialog.Builder(this);
-builder.setMessage("We disconnection! Cause: " + cause);
-builder.setTitle("Disconnected");
-
-builder.setPositiveButton("Reconnect?", new OnClickListener() {
-public void onClick(DialogInterface dialog, int which) {
-NetworkedActivity.this.network.reconnect(5, 1000);
-Toast.makeText(NetworkedActivity.this, "Trying to reconnect...", Toast.LENGTH_SHORT).show();
-}
-});
-
-builder.show();	
-}
-=======
 	private NetworkService network;	
 
 	/**
@@ -112,7 +28,7 @@ builder.show();
 	 * NOTE: This will return null before onNetworkServiceConnected is called!
 	 * @return a NetworkService.
 	 */
-	public NetworkService getNetworkService() {		
+	public NetworkService getNetworkService() {	
 		return this.network;
 	}
 
@@ -120,7 +36,7 @@ builder.show();
 	protected void onStart() {
 		//Bind the service so we can use it.
 		Intent intent = new Intent(this, NetworkService.class);
-		this.bindService(intent, connection, Context.BIND_WAIVE_PRIORITY);	  	
+		this.bindService(intent, connection, Context.BIND_WAIVE_PRIORITY);	
 		super.onStart();
 	}
 
@@ -143,7 +59,7 @@ builder.show();
 	private void addListeners() {
 		network.addConnectedListener(networkConnection);
 		network.addDisconnectedListener(networkDisconnected);
-		network.addRevicedListener(networkRecived);		
+		network.addRevicedListener(networkRecived);	
 	}
 
 	private Action networkConnection = new Action() {
@@ -192,7 +108,7 @@ builder.show();
 		//Dunno what to do here atm :O
 	}
 
-	protected  void onDisconnected(DCCause cause) {		
+	protected void onDisconnected(DCCause cause) {	
 		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setMessage("We disconnected! Cause: " + cause);
 		builder.setTitle("Disconnected");
@@ -204,19 +120,18 @@ builder.show();
 						if(success) {
 							Toast.makeText(NetworkedActivity.this, "Connected!", Toast.LENGTH_SHORT).show();
 						} else {
-							builder.setMessage("Failed to connect!");							
+							builder.setMessage("Failed to connect!");	
 							builder.show();
 						}
 					}
 				});
-				
+
 				Toast.makeText(NetworkedActivity.this, "Trying to reconnect...", Toast.LENGTH_SHORT).show();
 			}
 		});
 
-		builder.show();		
+		builder.show();	
 	}
->>>>>>> origin/master
 
 
 }
